34532b219eb2309270397ae2b53ad0326aee1076
Algoruu <zlfkcmspdbdp@naver.com>
2024-10-30 21:53:15 +0900

    인라인 스타일

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -3,26 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // let name = "리액트";
+  let name = "리액트";
+  const style = {
+    backgroundColor : 'black',
+    color : 'white',
+    fontSize : '48px',
+    fontWeight : 'bold',
+    padding : '20px'
+  }
 
-  // return (
-  //   <div className="container">
-  //     <h1 className='test'>Hello, 
-  //     {
-  //       name === '리액트' ? (<h1>YES</h1>): (<h1>NO</h1>) //null도 가능
-  //     }!!!</h1>
-  //     <p>반갑습니다.</p>
-  //   </div>
-  // );
-  const port = undefined;
-
-  return(
-    <div>
+  return (
+    <div style = {
       {
-        port || '포트를 설정하지 않았습니다.' // port || '3000' 일반적으로 포트 설정할 때 사용
+        backgroundColor : 'black',
+        color : 'white',
+        fontSize : '48px',
+        fontWeight : 'bold',
+        padding : '20px'
       }
+    }>
+      <h1 style = {style}>Hello, 
+      {
+        name === '리액트' ? (<h1>YES</h1>): (<h1>NO</h1>) // name === '리액트' ? (<h1>YES</h1>): null도 가능
+      }!!!</h1>
+      <p>반갑습니다.</p>
     </div>
-  )
+  );
+
+
+  // const port = undefined;
+  // return(
+  //   <div>
+  //     {
+  //       port || '포트를 설정하지 않았습니다.' // port || '3000' 일반적으로 포트 설정할 때 사용
+  //     }
+  //   </div>
+  // )
 
 }
 
