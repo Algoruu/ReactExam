e556bc2ddcbbb26e2e3c4343ed5a7d4777940fd5
Algoruu <zlfkcmspdbdp@naver.com>
2024-11-10 07:13:28 +0900

    props 사용

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Todolist from './Todolist';
 import Clock from './Timer';
+import MyWeather from './MyWeather';
 
 {/* 
   작성자 : kny
@@ -25,7 +26,8 @@ function App() {
   return (
     <div className='container'>
       <Todolist></Todolist>
-      <Clock></Clock>
+      {/* <Clock></Clock> */}
+      <MyWeather weather="맑음">일기예보</MyWeather>
     </div>
   );
 
